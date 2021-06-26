7d0b35f464a560b3288bca143dbeec01b81fd524
dasnonap <53471141+dasnonap@users.noreply.github.com>
2021-06-27 00:17:58 +0300

    Add column for Poster Image for Films table

---
+++ code_server/API/Migrations/20210626185427_AddPosterForMovies.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class AddPosterForMovies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "Poster",
+                table: "Films",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Poster",
+                table: "Films");
+        }
+    }
+}
---
+++ code_server/API/Migrations/20210626191636_RemovePosterImage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class RemovePosterImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Poster",
+                table: "Films");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Poster",
+                table: "Films",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+    }
+}
